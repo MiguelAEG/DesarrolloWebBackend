7ec9dadf20e604d9918a9151dc7d0277737d11db
Yuue0z <miguel.gk@gmail.com>
2023-11-03 08:23:30 -0600

    Eureka Client

---
+++ springboot-service-celularez/src/test/java/com/microservice/celularez/SpringbootServiceCelularezApplicationTests.java
@@ -0,0 +1,13 @@
+package com.microservice.celularez;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringbootServiceCelularezApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
