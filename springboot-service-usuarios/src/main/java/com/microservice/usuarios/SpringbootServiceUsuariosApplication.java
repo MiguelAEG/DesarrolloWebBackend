9ac8342a8d11d2452ff1cf68b2b25cb09f9b8d56
Yuue0z <miguel.gk@gmail.com>
2023-11-29 05:04:51 -0600

    configurando servicio usuarios commons

--- springboot-service-usuarios/src/main/java/com/microservice/usuarios/SpringbootServiceUsuariosApplication.java
+++ springboot-service-usuarios/src/main/java/com/microservice/usuarios/SpringbootServiceUsuariosApplication.java
@@ -2,7 +2,9 @@ package com.microservice.usuarios;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.domain.EntityScan;
 
+@EntityScan({"com.microservice.common.users.entity"})
 @SpringBootApplication
 public class SpringbootServiceUsuariosApplication {
 
