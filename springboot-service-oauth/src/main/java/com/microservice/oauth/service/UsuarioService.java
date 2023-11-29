ab3d06a4283510b0af37fe43437792e4dc165396
Yuue0z <miguel.gk@gmail.com>
2023-11-29 17:24:19 -0600

    UserDetailService con FeignClient

---
+++ springboot-service-oauth/src/main/java/com/microservice/oauth/service/UsuarioService.java
@@ -0,0 +1,45 @@
+package com.microservice.oauth.service;
+
+import java.util.List;
+import java.util.stream.Collector;
+import java.util.stream.Collectors;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
+import org.springframework.security.core.userdetails.User;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+
+import com.microservice.common.users.entity.Usuario;
+import com.microservice.oauth.client.UsuarioFeignClient;
+
+public class UsuarioService implements UserDetailsService{
+
+	private Logger log = LoggerFactory.getLogger(UsuarioService.class);
+	@Autowired
+	private UsuarioFeignClient client;
+	
+	@Override
+	public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
+		
+		Usuario user = client.findByUsername(username);
+		
+		if(user==null) {
+			log.error("Error en el login, no existe el uuario. "+ username);
+			throw new UsernameNotFoundException("Error en el login, no existe el uuario. "+ username);
+		}
+		
+		List<GrantedAuthority> autorities = user.getRoles()
+				.stream()
+				.map(role-> new SimpleGrantedAuthority(role.getNombre()))
+				.peek(authority-> log.info("Role:" +authority.getAuthority()))
+				.collect(Collectors.toList());
+		log.info("Usuario auytentificado: "+ username);
+		return new User(user.getUsername(), user.getPassword(), user.getEneable(), true, true, true, autorities);
+	}
+
+}
